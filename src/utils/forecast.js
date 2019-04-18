d59e28da4073ed2bcc08f1cfc22fe1ac0a1e9963
vexan90 <vexan90@users.noreply.github.com>
2019-04-17 21:55:06 -0700

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,5 @@
 const request = require('request');
 
-
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/9ceae926ef024d0758286c2d55dc83e5/' + latitude + ',' + longitude;
 
@@ -10,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.');
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.');
         }
     })
 }
